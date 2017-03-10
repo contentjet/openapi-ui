25c82394ab9c1d66a9a06a9fc290a60c2a363605
Will Dady <willdady@gmail.com>
2017-03-10 23:19:01 +1100

    Updated license component.

--- src/lib/components/openapi/License/License.jsx
+++ src/lib/components/openapi/License/License.jsx
@@ -1,14 +1,25 @@
 import React, { PropTypes } from 'react';
 import classnames from 'classnames';
+import List from 'lib/components/common/List';
+import Heading from 'lib/components/common/Heading';
+import LabelValueListItem from 'lib/components/common/LabelValueListItem';
 import s from './License.css';
 
 
 function License(props) {
   const className = classnames(s.license, props.className);
   return (
-    <div className={className}>
-
-    </div>
+    <section className={className}>
+      <Heading level="h3">License</Heading>
+      <List>
+        <LabelValueListItem label="Name">
+          { props.name }
+        </LabelValueListItem>
+        <LabelValueListItem label="URL">
+          <a href={props.url}>{ props.url }</a>
+        </LabelValueListItem>
+      </List>
+    </section>
   );
 }
 License.propTypes = {
